05536033f734d32282ba6f355936e8b6ce1b6023
Patryk Palej <patrykpalej@gmail.com>
2024-01-31 09:17:54 +0100

    Update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705525812';
+const cacheName = 'chirpy-1706689068';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
